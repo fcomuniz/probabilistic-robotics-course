630dcb4a31b53363059779c96648ab7de8309b2c
Francisco Muniz <munizfco@gmail.com>
2017-03-01 00:22:38 -0300

    Fixing the CmakeLists, putting the qcustomplot and Creating the executable for DiscreteWorld gui

--- source/core/gui/DiscreteWorldGui.cpp
+++ source/core/gui/DiscreteWorldGui.cpp
@@ -3,3 +3,4 @@
 //
 
 #include "DiscreteWorldGui.h"
+
--- source/tests/unit/robot/DifferentialRobot_UnitTest.cpp
+++ source/tests/unit/robot/DifferentialRobot_UnitTest.cpp
@@ -3,15 +3,17 @@
 //
 
 #include "DifferentialRobot_UnitTest.h"
+#include "math/Pose2D.h"
 
 using ::robot::DifferentialRobot;
+using itandroids_lib::math::Pose2D;
 
 typedef ::testing::Types<int,double,float> MyTypes;
 TYPED_TEST_CASE(DifferentialRobot_UnitTest, MyTypes);
 
 TYPED_TEST(DifferentialRobot_UnitTest, initialization_test){
 
-    DifferentialRobot robot;
+    DifferentialRobot robot(Pose2D());
     ASSERT_DOUBLE_EQ(robot.position.translation.x, 0);
     ASSERT_DOUBLE_EQ(robot.position.translation.y, 0);
 }
